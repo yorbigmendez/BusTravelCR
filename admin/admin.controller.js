a125f24ef1442083f694c13d0d380df9b59713f0
Yorbi Mendez <yorbigmendez@gmail.com>
2016-06-14 01:05:21 -0600

    Adding administrative views

--- admin/admin.controller.js
+++ admin/admin.controller.js
@@ -1,7 +1,11 @@
 app.controller("adminController",function($scope, $http){
+    $scope.showDialog = false;
     $scope.busTerminal = "";
     $scope.addTerminal = function(){
         $scope.busTerminal = "THis has been updated by click";
+    };
+    $scope.insertRoute = function(){
+        console.log('Hola Yorbi');
     };
     $scope.changeView = function(view){
         if(view == 'maps'){
@@ -25,4 +29,25 @@ app.controller("adminController",function($scope, $http){
             console.log('On move right');
         }  
     };
+    $scope.zoom = function(zoom){
+         if(zoom == 'in'){
+            console.log('On Zoom In');
+        } if(zoom == 'out'){
+            console.log('On Zoom Out');
+        }
+    };
+    $scope.select = {
+            value: "Option1",
+            choices: ["Option1", "I'm an option", "This is materialize", "No, this is Patrick."]
+        };
+    $scope.showDialog = function(){
+        console.log("Going to change attribute");
+          $scope.showDialog = true;
+    };
+});
+
+app.run(function ($rootScope){
+    angular.element(document).on("click",function(e){
+        $rootScope.$broadcast("documentClicked",angular.element(e.target));
+    });
 });
